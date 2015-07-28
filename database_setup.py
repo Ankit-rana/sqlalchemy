5a82a44a5a7961ecd0f4cf1b5a9aa35d187d8892
root <root@localhost.localdomain>
2015-07-28 12:28:58 +0530

    table creation script

---
+++ database_setup.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine,Column,Integer,String
+
+#object of declarative_base class so that class can be connected to Base hierarchy
+Base = declarative_base()
+
+
+#make a new class which will be mapped to table in db
+class User(Base):
+	__tablename__= 'users'
+	id = Column(Integer,primary_key=True)
+	name = Column(String)
+	password = Column(String)
+	def __repr__(self):
+		return "name:'%s'"%(self.name)
+
+
+#connect to a database
+engine=create_engine('sqlite:///:memory:',echo=True)
+
+
+#now make class into the table in db
+Base.metadata.create_all(engine)
