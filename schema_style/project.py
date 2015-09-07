7328d41e40a179a334bda674ed0574da5645cad2
anky <anky@localhost.localdomain>
2015-09-07 11:49:54 +0530

    schema style

---
+++ schema_style/project.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine,Table,Integer,String,Table,Column,MetaData
+
+#lazy connection
+engine = create_engine('sqlite:///users.db')
+
+#create metdata and create all tables in it
+meta=MetaData()
+
+users = Table('users',meta,
+	Column('uid',Integer,primary_key=True),
+	Column('name',String(150))
+	)
+
+#users.create(engine)
+# OR 
+#bind the connection to metadata and create
+meta.bind=engine
+users.create()
